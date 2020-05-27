bbe907cd91a6d9432693cb53b65de80771243fec
iv <consmebius@gmail.com>
2020-05-27 11:23:22 +0300

    .

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
+// var url = 'http://newsapi.org/v2/top-headlines?' +
+//           'country=us&' +
+//           'apiKey=30c7f43b252741f2b6b6b7d12b486cea';
+// var req = new Request(url);
+// fetch(req, { referrer: location.href })
+//     .then(function(response) {
+//         console.log(response.json());
+//     })
+
+var url = 'http://newsapi.org/v2/top-headlines?' +
+          'sources=bbc-news&' +
           'apiKey=30c7f43b252741f2b6b6b7d12b486cea';
 var req = new Request(url);
-fetch(req, { referrer: location.href })
+fetch(req)
     .then(function(response) {
         console.log(response.json());
     })
